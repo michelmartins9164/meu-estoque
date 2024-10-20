f7acf106a242af833d17f9ccb12e8cdb06a14087
Michel Martins <michelmartins9164@gmail.com>
2024-10-20 13:51:31 -0300

    feat: implementando validação de formulario

--- src/ui/pages/Login/index.tsx
+++ src/ui/pages/Login/index.tsx
@@ -1,21 +1,29 @@
-import { Box, Button, Flex, Input, InputGroup, InputLeftElement, Text, useToast } from '@chakra-ui/react'
+import { Box, Button, Flex, FormControl, FormErrorMessage, Input, InputGroup, InputLeftElement, Text, useToast } from '@chakra-ui/react'
 import { signInWithEmailAndPassword } from 'firebase/auth'
-import { FormEvent } from 'react'
-
+import { useForm } from "react-hook-form"
 import { auth } from '../../../api/firebase'
 import password from '../../../assets/password.svg'
 import user from '../../../assets/user.svg'
 import { useNavigate } from 'react-router-dom'
-
+type FormData = {
+    email: string
+    password: string
+}
 export default function Login() {
     const navigate = useNavigate()
     const toast = useToast()
-   async function SingIn(e: FormEvent) {
-        e.preventDefault()
+    const { register, handleSubmit, formState: { errors } } = useForm<FormData>()
+    async function SingIn({
+        email,
+        password
+    }: {
+        email: string
+        password: string
+    }) {
         try {
-            await signInWithEmailAndPassword(auth, 'michelmartins9164@gmail.com', '123456') 
+            await signInWithEmailAndPassword(auth, email, password)
             navigate('/home')
-        } catch(err) {
+        } catch (err) {
             console.log(err)
             toast({
                 title: 'Erro ao fazer login',
@@ -24,7 +32,7 @@ export default function Login() {
                 duration: 5000,
                 isClosable: true,
             })
-        }   
+        }
     }
     return (
         <Flex w={'100%'} h={'100vh'} bg={'#151316'} position={'relative'} align={'end'}>
@@ -47,52 +55,67 @@ export default function Login() {
                 justifyContent={'center'}
             >
                 <Flex justifyContent={'center'} align={'center'}>
-                    <form onSubmit={(e)=>SingIn(e)}>
+                    <form onSubmit={handleSubmit((data) => SingIn({
+                        email: data.email,
+                        password: data.password
+                    }))}>
                         <Flex flexDir={'column'} w={'100%'} justifyContent={'center'} align={'center'}>
                             <Flex align={'center'} mb={4}>
-                                <InputGroup alignItems={'center'}>
-                                    <InputLeftElement pointerEvents='none' h={'40px'} pl={2}>
-                                        <Box as="img" src={user} w={6} h={6} />
-                                    </InputLeftElement>
-                                    <Input
-                                        pl={10}
-                                        placeholder="Usuário"
-                                        w={'300px'}
-                                        h={'40px'}
-                                        bg={'#151316'}
-                                        color={'#A4A4A4'}
-                                        borderRadius={'6px'}
-                                        border={'1px solid #A4A4A4'}
-                                        _focus={{
-                                            borderColor: '#ffffff',
-                                            bg: '#151316'
-                                        }}
-                                        colorScheme="purple"
-                                    />
-                                </InputGroup>
+                                <FormControl isInvalid={!!errors.email}>
+                                    <InputGroup alignItems={'center'}>
+                                        <InputLeftElement pointerEvents='none' h={'40px'} pl={2}>
+                                            <Box as="img" src={user} w={6} h={6} />
+                                        </InputLeftElement>
+                                        <Input
+                                        {...register('email', { required: 'Campo obrigatório' })}
+                                            pl={10}
+                                            placeholder="Usuário"
+                                            w={'300px'}
+                                            h={'40px'}
+                                            bg={'#151316'}
+                                            color={'#A4A4A4'}
+                                            borderRadius={'6px'}
+                                            border={'1px solid #A4A4A4'}
+                                            _focus={{
+                                                borderColor: '#ffffff',
+                                                bg: '#151316'
+                                            }}
+                                            colorScheme="purple"
+                                        />
+                                    </InputGroup>
+                                    <FormErrorMessage>
+                                        {errors.email?.message}
+                                    </FormErrorMessage>
+                                </FormControl>
                             </Flex>
                             <Flex align={'center'}>
-                                <InputGroup alignItems={'center'}>
-                                    <InputLeftElement pointerEvents='none' h={'40px'} pl={2}>
-                                        <Box as="img" src={password} w={6} h={6} />
-                                    </InputLeftElement>
-                                    <Input
-                                        type="password"
-                                        variant={'unstyled'}
-                                        pl={10}
-                                        placeholder="Senha"
-                                        w={'300px'}
-                                        h={'40px'}
-                                        bg={'#151316'}
-                                        color={'#A4A4A4'}
-                                        borderRadius={'6px'}
-                                        border={'1px solid #A4A4A4'}
+                                <FormControl isInvalid={!!errors.password}>
+                                    <InputGroup alignItems={'center'}>
+                                        <InputLeftElement pointerEvents='none' h={'40px'} pl={2}>
+                                            <Box as="img" src={password} w={6} h={6} />
+                                        </InputLeftElement>
+                                        <Input
+                                        {...register('password', { required: 'Campo obrigatório' })}
+                                            type="password"
+                                            variant={'unstyled'}
+                                            pl={10}
+                                            placeholder="Senha"
+                                            w={'300px'}
+                                            h={'40px'}
+                                            bg={'#151316'}
+                                            color={'#A4A4A4'}
+                                            borderRadius={'6px'}
+                                            border={'1px solid #A4A4A4'}
 
-                                        _focus={{
-                                            borderColor: '#ffffff'
-                                        }}
-                                    />
-                                </InputGroup>
+                                            _focus={{
+                                                borderColor: '#ffffff'
+                                            }}
+                                        />
+                                    </InputGroup>
+                                    <FormErrorMessage>
+                                        {errors.password?.message}
+                                    </FormErrorMessage>
+                                </FormControl>
                             </Flex>
                         </Flex>
                         <Flex mt={10} w={'100%'} justifyContent={'center'}>
